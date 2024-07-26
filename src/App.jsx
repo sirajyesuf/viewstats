bb44f91bac6aeac4e65c1b5fefa2a0ee74b743a8
siraj yesuf <sirajyesuf762@gmail.com>
2024-07-26 21:47:02 +0300

    =refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,66 @@
 import { useState } from 'react'
-// import defaultVideos from './db/videos'
 
 function App() {
-  const [v1,setV1] = useState('')
-  const [v2,setV2] = useState('')
-  const [v1Detail,setV1Detail] = useState('')
-  const [v2Detail,setV2Detail] = useState('')
 
-  // useEffect(()  =>  {
+  const [v1,setV1] = useState({
+    id: '',
+    title: '',
+    views: '',
+    likes: '',
+    thumbnail_url: ''
+  })
 
-  //   let dv = defaultVideos();
+  const [v2,setV2] = useState({
+    id: '',
+    title: '',
+    views: '',
+    likes: '',
+    thumbnail_url: ''
+})
 
-  //   dv.forEach(async function(index,value){
-  //     if(index == 0 ){
+async function setVideo1(videoId){
+    getVedioDetail(videoId).then((data) => {
+      setV1(data)
+    })
+}
 
-  //       setV1(value)
-  //       let r = await getVedioDetail2(value)
-  //       setV1Detail(r)
-  //     }
-  //     if(index == 1){
-  //       setV2(value)
-  //       let r = await getVedioDetail2(value)
-  //       setV2Detail(r)
-  //     }
-  //   });
-
-  //   async function getVedioDetail2(videoId){
-
-  //     const url = new URL('https://youtube.googleapis.com/youtube/v3/videos');
-  //     url.searchParams.append('part', 'snippet,statistics');
-  //     url.searchParams.append('id', videoId);
-  //     url.searchParams.append('key',"AIzaSyAJ_cBZSmElM_7hPCW_3DVFmPBl9mYhe9U");
-  //     const headers = new Headers();
-  //     headers.append('Accept', 'application/json');
-  
-  //    const response = await fetch(url, {
-  //       method: 'GET',
-  //       headers: headers
-  //     });
-  
-  //     return await response.json();
-  //   }
+async function setVideo2(videoId){
+    getVedioDetail(videoId).then((data) => {
+      setV2(data)
+    })
+}
 
 
+  async function getVedioDetail(videoId){
 
-  // }, []);
+        const url = new URL('https://youtube.googleapis.com/youtube/v3/videos');
+        url.searchParams.append('part', 'snippet,statistics');
+        url.searchParams.append('id', videoId);
+        url.searchParams.append('key',"AIzaSyAJ_cBZSmElM_7hPCW_3DVFmPBl9mYhe9U");
+        const headers = new Headers();
+        headers.append('Accept', 'application/json');
 
-
-
-  async function getVedioDetail(videoId,isvideo1=true){
-
-      const url = new URL('https://youtube.googleapis.com/youtube/v3/videos');
-      // url.searchParams.append('part','statistics');
-            url.searchParams.append('part', 'snippet,statistics');
-      url.searchParams.append('id', videoId);
-      url.searchParams.append('key',"AIzaSyAJ_cBZSmElM_7hPCW_3DVFmPBl9mYhe9U");
-      const headers = new Headers();
-      headers.append('Accept', 'application/json');
-
-     const response = await fetch(url, {
+        const response = await fetch(url, {
         method: 'GET',
         headers: headers
-      });
+        });
 
-    const result = await response.json();
+        const result = await response.json();
 
-    if(isvideo1 == true){
-      setV1Detail(result)
-    }
-    else{
-      setV2Detail(result)
-    }
-
-  }
-
-  function setVideo1(v1){
-    setV1(v1)
-    getVedioDetail(v1)
-  }
-
-  function setVideo2(v2){
-    setV2(v2)
-    getVedioDetail(v2,false)
+        return {
+          id: videoId,
+          title: result.items[0].snippet.title,
+          views: result.items[0].statistics.viewCount,
+          likes: result.items[0].statistics.likeCount,
+          comment: result.items[0].statistics.commentCount,
+          thumbnail_url: result.items[0].snippet.thumbnails.high.url
+        }
 
   }
 
   return (
     <>
-    <div className='flex justify-between items-center px-12 py-2 bg-white'>
+    <div className='flex justify-between items-center px-12 py-2 bg-[#faf9fd]'>
       <div className='w-10 h-10'>
         <a href="/" className='flex items-center gap-2'>
           <img src="./public/yt.png"/>
@@ -95,20 +68,21 @@ function App() {
         </a>
       </div>
     </div>
-    <div className="bg-[#fafcff] min-h-screen mx-auto mt-8 flex flex-col gap-8 font-black px-28 py-8">
+
+    <div className="min-h-screen mx-auto mt-8 flex flex-col gap-8 font-black px-28 py-8">
 
       <div className="rounded-md p-4 flex justify-around items-center gap-3">
 
         <div className="w-2/4">
-        <input type="text" value={v1} className="bg-[#f1f3f4] p-4 rounded-full w-full"
+        <input type="text" value={v1.id} className="bg-[#f1f3f4] p-4 rounded-full w-full"
         onChange={ e => setVideo1(e.target.value)}
         />
         </div>
 
-        <p className="font-normal text-[#383838]">VS</p>
+        <p className="text-white border-2 border-[#ff0000] rounded-full p-2 bg-[#ff0000] flex items-center">VS</p>
 
         <div className="w-2/4">
-        <input type="text"  value={v2} className="bg-[#f1f3f4] p-4 rounded-full w-full" 
+        <input type="text"  value={v2.id} className="bg-[#f1f3f4] p-4 rounded-full w-full" 
         onChange={e => setVideo2(e.target.value)}
         />
         </div>
@@ -119,41 +93,41 @@ function App() {
 
         <div className="flex gap-8 p-4 border-none h-full">
 
-        { v1 != '' & v1Detail != '' && (
+        { v1.id  && (
 
 
-                <div className='border-2 border-[#ff0000] rounded-lg p-4 w-2/4'>
+                <div className='border-2 border-[#ff0000] rounded-lg p-4 w-2/4 flex flex-col justify-between'>
+                  
                   <div className='p-2 flex gap-4'>
+
+                    <div className='flex gap-4 w-1/4'>
+
                       <div className='border-8 border-[#ff0000] rounded-lg self-stretch'></div>
                       <div className='w-40 h-40'>
-                        <img src={v1Detail.items[0].snippet.thumbnails.high.url} alt="" className='w-full h-full rounded-lg bg-cover' />
+                      <img src={v1.thumbnail_url} alt="" className='w-full h-full rounded-lg object-cover' />
                       </div>
-                      <h1 className='text-capital font-extrabold py-8 text-3xl'>
-                        {v1Detail.items[0].snippet.title}
-                      </h1>
+
+                    </div>
+
+                    <div className='font-extrabold py-8 text-3xl w-3/4'>{v1.title}</div>
+
                   </div>
 
                   <div className='flex justify-evenly py-4'>
 
                     <div className='flex flex-col items-center'>
                       <p className='font-bold text-xl'>Total View</p>
-                      <p>
-                        {v1Detail.items[0].statistics.viewCount}
-                      </p>
+                      <p>{v1.views}</p>
                     </div>
 
                     <div className='flex flex-col items-center'>
                       <p className='font-bold text-xl'>Likes</p>
-                      <p>
-                        {v1Detail.items[0].statistics.likeCount}
-                      </p>
+                      <p>{v1.likes}</p>
                     </div>
 
                     <div className='flex flex-col items-center'>
                       <p className='font-bold text-xl'>Comment</p>
-                      <p>
-                        {v1Detail.items[0].statistics.commentCount}
-                      </p>
+                      <p>{v1.comment}</p>
                     </div>
 
                   </div>
@@ -161,17 +135,25 @@ function App() {
                 </div>
           )}
 
-          { v2 !='' & v2Detail != '' && (
+          { v2.id  && (
 
-          <div className='border-2 border-[#0047ff] rounded-lg p-4 w-2/4'>
+          <div className='border-2 border-[#0047ff] rounded-lg p-4 w-2/4 flex flex-col justify-between'>
+
           <div className='p-2 flex gap-4'>
+
+            <div className='flex gap-4 w-1/4'>
+
               <div className='border-8 border-[#0047ff] rounded-lg self-stretch'></div>
               <div className='w-40 h-40'>
-              <img src={v2Detail.items[0].snippet.thumbnails.high.url} alt="" className='w-full h-full rounded-lg bg-cover' />
+              <img src={v2.thumbnail_url} alt="" className='w-full h-full rounded-lg object-cover' />
               </div>
-              <div className='font-extrabold py-8 text-3xl'>
-              {v2Detail.items[0].snippet.title}
-              </div>
+
+            </div>
+
+            <div className='font-extrabold py-8 text-3xl w-3/4'>
+            {v2.title}
+            </div>
+
           </div>
             
             <div className='flex justify-evenly py-4'>
@@ -179,21 +161,21 @@ function App() {
             <div className='flex flex-col items-center'>
               <p className='font-bold text-xl'>Total View</p>
               <p>
-                {v2Detail.items[0].statistics.viewCount}
+                {v2.views}
               </p>
             </div>
 
             <div className='flex flex-col items-center'>
               <p className='font-bold text-xl'>Likes</p>
               <p>
-                {v2Detail.items[0].statistics.likeCount}
+                {v2.likes}
               </p>
             </div>
 
             <div className='flex flex-col items-center'>
               <p className='font-bold text-xl'>Comment</p>
               <p>
-                {v2Detail.items[0].statistics.commentCount}
+                {v2.comment}
               </p>
             </div>
 
